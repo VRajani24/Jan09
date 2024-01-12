334811e96ff6191eb3ead41c5cc20e753033abfc
Rajani Varala <varalarajani24@gmail.com>
2024-01-12 14:23:29 +0530

    'first'

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jan09_consoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Console Application");
+            Console.ReadLine(); 
+        }
+    }
+}
